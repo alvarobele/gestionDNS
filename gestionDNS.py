ba8086cade37abb4c2590e8b51cd3a13cf421d81
Álvaro Beleño <alvarobrod@outlook.es>
2018-11-30 09:56:11 +0100

    Arreglados errores en el funcionamiento del script

--- gestionDNS.py
+++ gestionDNS.py
@@ -1,8 +1,5 @@
 import sys
 
-# Si el script recibe -a, se añadirá el nombre que recibe
-# Si el script recibe -b, se borrará el nombre que recibe 
-
 # Aquí irá la ruta a nuestro fichero
 path = 'prueba'
 
@@ -12,7 +9,7 @@ lineas = f.readlines()
 f.close()
 
 # Abrimos el fichero con el modo a+ para que no nos borre todo el contenido que teníamos previamente
-f = open(path, 'w')
+f = open(path, 'a')
 
 # Comprobamos los argumentos que le estamos pasando al programa
 if sys.argv[1] == '-a':
@@ -22,11 +19,10 @@ if sys.argv[1] == '-a':
     # Si los argumentos son -a y -alias, creamos un registro de tipo CNAME    
     elif sys.argv[2] == '-alias':
         f.writelines(['\n{}\t\tCNAME\t\t{}'.format(sys.argv[3], sys.argv[4])])
+    f.close()
 elif sys.argv[1] == '-b':
     # Si el argumento es -b, borramos el registro con el nombre que le hayamos pasado
-    for linea in lineas:
-        if sys.argv[2] not in linea:
-            f.writelines(linea)
-
-
-
+    with open(path, 'w') as fich:
+        for linea in lineas:
+            if sys.argv[2] not in linea:
+                fich.writelines(linea)
