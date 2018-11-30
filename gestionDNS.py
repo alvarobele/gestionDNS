ae5a5bac2bb7e89e21e8355996242c37dbd87908
Álvaro Beleño <alvarobrod@outlook.es>
2018-11-30 10:02:38 +0100

    Mejorado funcionamiento del script

--- gestionDNS.py
+++ gestionDNS.py
@@ -15,10 +15,10 @@ f = open(path, 'a')
 if sys.argv[1] == '-a':
     # Si los argumentos son -a y -dir, creamos un registro de tipo A
     if sys.argv[2] == '-dir':
-        f.writelines(['\n{}\t\tA\t\t{}'.format(sys.argv[3], sys.argv[4])])
+        f.writelines(['\n{}.alvaro.gonzalonazareno.org.\t\tA\t\t{}'.format(sys.argv[3], sys.argv[4])])
     # Si los argumentos son -a y -alias, creamos un registro de tipo CNAME    
     elif sys.argv[2] == '-alias':
-        f.writelines(['\n{}\t\tCNAME\t\t{}'.format(sys.argv[3], sys.argv[4])])
+        f.writelines(['\n{}.alvaro.gonzalonazareno.org.\t\tCNAME\t\t{}'.format(sys.argv[3], sys.argv[4])])
     f.close()
 elif sys.argv[1] == '-b':
     # Si el argumento es -b, borramos el registro con el nombre que le hayamos pasado
