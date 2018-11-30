2413d650a670f51050ed2c69928d5e759e99801e
Álvaro Beleño <alvarobrod@outlook.es>
2018-11-30 10:23:28 +0100

    Cambios en conf.csv y en el script

--- gestionDNS.py
+++ gestionDNS.py
@@ -1,5 +1,9 @@
 import sys
 
+# Abrimos el fichero de configuración y guardamos el contenido
+with open('conf.csv', 'r') as c:
+   cfg = c.readlines().pop(1).strip('\n').split(',')
+
 # Aquí irá la ruta a nuestro fichero
 path = 'prueba'
 
@@ -8,18 +12,18 @@ f = open(path, 'r')
 lineas = f.readlines()
 f.close()
 
-# Abrimos el fichero con el modo a+ para que no nos borre todo el contenido que teníamos previamente
+# Abrimos el fichero con el modo a para que no nos borre todo el contenido que teníamos previamente
 f = open(path, 'a')
 
 # Comprobamos los argumentos que le estamos pasando al programa
 if sys.argv[1] == '-a':
     # Si los argumentos son -a y -dir, creamos un registro de tipo A
     if sys.argv[2] == '-dir':
-        f.writelines(['\n{}.alvaro.gonzalonazareno.org.\t\tA\t\t{}'.format(sys.argv[3], sys.argv[4])])
+        f.writelines(['\n{}.{}\t\tA\t\t{}'.format(sys.argv[3], cfg[0] , sys.argv[4])])
         
     # Si los argumentos son -a y -alias, creamos un registro de tipo CNAME    
     elif sys.argv[2] == '-alias':
-        f.writelines(['\n{}.alvaro.gonzalonazareno.org.\t\tCNAME\t\t{}'.format(sys.argv[3], sys.argv[4])])
+        f.writelines(['\n{}.{}\t\tCNAME\t\t{}'.format(sys.argv[3], cfg[0], sys.argv[4])])
 
     f.close()
 
