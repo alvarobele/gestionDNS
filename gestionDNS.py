44ce2062fd5d33ca0dd63e12f9e509092cdc79d5
Álvaro Beleño <alvarobrod@outlook.es>
2018-11-30 09:43:54 +0100

    Añadida función de borrar

--- gestionDNS.py
+++ gestionDNS.py
@@ -6,6 +6,11 @@ import os
 # Aquí irá la ruta a nuestro fichero
 path = 'prueba'
 
+f = open(path, 'r')
+
+lineas = f.readlines()
+f.close()
+
 # Abrimos el fichero con el modo a+ para que no nos borre todo el contenido que teníamos previamente
 f = open(path, 'a+')
 
@@ -17,3 +22,11 @@ if sys.argv[1] == '-a':
     # Si los argumentos son -a y -alias, creamos un registro de tipo CNAME    
     elif sys.argv[2] == '-alias':
         f.writelines(['\n{}\t\tCNAME\t\t{}'.format(sys.argv[3], sys.argv[4])])
+elif sys.argv[1] == '-b':
+    # Si el argumento es -b, borramos el registro con el nombre que le hayamos pasado
+    for linea in lineas:
+        if sys.argv[2] not in linea:
+            f.writelines(linea)
+
+
+
