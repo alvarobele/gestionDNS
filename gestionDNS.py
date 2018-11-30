eeb67b7a536f7c31bfd6dbb29cebd4b84e5f6e70
Álvaro Beleño <alvarobrod@outlook.es>
2018-11-30 09:18:04 +0100

    Añadida distinción por parámetros

--- gestionDNS.py
+++ gestionDNS.py
@@ -3,3 +3,17 @@ import os
 # Si el script recibe -a, se añadirá el nombre que recibe
 # Si el script recibe -b, se borrará el nombre que recibe 
 
+# Aquí irá la ruta a nuestro fichero
+path = 'prueba'
+
+# Abrimos el fichero con el modo a+ para que no nos borre todo el contenido que teníamos previamente
+f = open(path, 'a+')
+
+# Comprobamos los argumentos que le estamos pasando al programa
+if sys.argv[1] == '-a':
+    # Si los argumentos son -a y -dir, creamos un registro de tipo A
+    if sys.argv[2] == '-dir':
+        f.writelines(['\n{}\t\tA\t\t{}'.format(sys.argv[3], sys.argv[4])])
+    # Si los argumentos son -a y -alias, creamos un registro de tipo CNAME    
+    elif sys.argv[2] == '-alias':
+        f.writelines(['\n{}\t\tCNAME\t\t{}'.format(sys.argv[3], sys.argv[4])])
